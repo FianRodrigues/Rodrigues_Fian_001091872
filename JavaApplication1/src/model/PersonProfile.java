a0785a0ed0a7d939ebb442f27c3ccbc0fcf730a4
FianRodrigues <91443480+FianRodrigues@users.noreply.github.com>
2021-09-26 23:48:17 -0400

    Add files via upload

---
+++ JavaApplication1/src/model/PersonProfile.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.net.InetAddress;
+import java.util.Date;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author fianrodrigues
+ */
+public class PersonProfile 
+{
+private String name;
+private String geographicalData;
+private String dateOfbirth;
+private Long telephoneNumber;
+private Long faxNumber;
+private String emailAddress;
+private int socialSecuritynumber;
+private Long medicalRecordnumber;
+private Long beneficiaryNumber;
+private Long accountNumber;
+private Long licenseNumber;
+private String plateNumber;
+private String deviceIdentifier;
+private String linkedinLink;
+private String iPaddress;
+private Long indentifyingNum;
+private ImageIcon picture;
+private ImageIcon image2;
+
+    public ImageIcon getImage2() {
+        return image2;
+    }
+
+    public void setImage2(ImageIcon image2) {
+        this.image2 = image2;
+    }
+
+    public ImageIcon getPicture() {
+        return picture;
+    }
+
+    public void setPicture(ImageIcon picture) {
+        this.picture = picture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String Name) {
+        this.name = Name;
+    }
+
+    public String getGeographicalData() {
+        return geographicalData;
+    }
+
+    public void setGeographicalData(String GeographicalData) {
+        this.geographicalData = GeographicalData;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfbirth;
+    }
+
+    public void setDateOfBirth(String DateOfBirth) {
+        this.dateOfbirth = DateOfBirth;
+    }
+
+    public Long getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(Long TelephoneNumber) {
+        this.telephoneNumber = TelephoneNumber;
+    }
+
+    public Long getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(Long FaxNumber) {
+        this.faxNumber = FaxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String EmailAddress) {
+        this.emailAddress = EmailAddress;
+    }
+
+    public int getSocialSecurityNumber() {
+        return socialSecuritynumber;
+    }
+
+    public void setSocialSecurityNumber(int SocialSecurityNumber) {
+        this.socialSecuritynumber = SocialSecurityNumber;
+    }
+
+    public Long getMedicalRecordNumber() {
+        return medicalRecordnumber;
+    }
+
+    public void setMedicalRecordNumber(Long MedicalRecordNumber) {
+        this.medicalRecordnumber = MedicalRecordNumber;
+    }
+
+    public Long getBeneficiaryNumber() {
+        return beneficiaryNumber;
+    }
+
+    public void setBeneficiaryNumber(Long BeneficiaryNumber) {
+        this.beneficiaryNumber = BeneficiaryNumber;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long AccountNumber) {
+        this.accountNumber = AccountNumber;
+    }
+
+    public Long getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(Long LicenseNumber) {
+        this.licenseNumber = LicenseNumber;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String PlateNumber) {
+        this.plateNumber = PlateNumber;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String DeviceIdentifier) {
+        this.deviceIdentifier = DeviceIdentifier;
+    }
+
+    public String getLinkedinLink() {
+        return linkedinLink;
+    }
+
+    public void setLinkedinLink(String LinkedinLink) {
+        this.linkedinLink = LinkedinLink;
+    }
+
+    public String getIPAddress() {
+        return iPaddress;
+    }
+
+    public void setIPAddress(String IPAddress) {
+        this.iPaddress = IPAddress;
+    }
+
+    public Long getIndentifyingnum() {
+        return indentifyingNum;
+    }
+
+    public void setIndentifyingnum(Long Indentifyingnum) {
+        this.indentifyingNum = Indentifyingnum;
+    }
+
+    
+
+
+}
