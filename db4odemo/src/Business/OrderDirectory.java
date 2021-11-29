2a02c1bac4b40ec5e7371d80cdedfb58fd78fe7d
FianRodrigues <91443480+FianRodrigues@users.noreply.github.com>
2021-11-29 09:22:52 -0500

    Revert "Delete OrderDirectory.java"
    This reverts commit d4d56b25c14c7b452286ffe131815648b1b58143.

---
+++ db4odemo/src/Business/OrderDirectory.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fianrodrigues
+ */
+public class OrderDirectory
+{
+     ArrayList<Order> orderList = new ArrayList<>();
+    
+    public OrderDirectory()
+    {
+        
+    }
+
+    public void addOrder(Order order)
+    {
+        try
+        {
+            orderList.add(order);
+        }
+        catch(Exception e)
+        {
+            orderList = new ArrayList<>();
+            orderList.add(order);
+        }
+        
+    }
+    
+    public ArrayList<Order> getOrderList() 
+    {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<Order> orderList) 
+    {
+        this.orderList = orderList;
+    }
+    
+    
+}
